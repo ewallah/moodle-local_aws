bbee042e180707602b392c8a31fa3417701aa3e5
Renaat Debleu <rdebleu@eWallah.net>
2024-11-17 05:07:02 +0000

    3.328.0

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->release = "3.325.5";
+$plugin->release = "3.328.0";
 $plugin->version = 2024180800;
 // $plugin->requires = 2024100100;
 $plugin->component = 'local_aws';
